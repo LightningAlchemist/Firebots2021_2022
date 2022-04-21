8c4f0ff2c5ab0001339cbaaba9e3e05315228f7c
Joseph <joseph.casebeer@gmail.com>
2022-04-21 11:11:52 -0700

    Built a new package called brian_tools which holds the launch file and the output file

---
+++ build/brian_tools/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/joseph/catkin_ws/src/brian_tools
+# Build directory: /home/joseph/catkin_ws/build/brian_tools
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
---
+++ devel/share/brian_tools/cmake/brian_toolsConfig.cmake
@@ -0,0 +1,1 @@
+/home/joseph/catkin_ws/devel/.private/brian_tools/share/brian_tools/cmake/brian_toolsConfig.cmake
